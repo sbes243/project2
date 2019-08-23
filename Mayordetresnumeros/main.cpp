713175154125409c972a15916293633c1501890f
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2019-08-23 14:51:48 -0500

    La tarea que tenia que hacer

--- Mayordetresnumeros/main.cpp
+++ Mayordetresnumeros/main.cpp
@@ -4,6 +4,25 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int a;
+    int b;
+    int c;
+    cout << "Ingresa el valor de a "  << endl;
+    cin >> a;
+    cout << "Ingresa el valor de b " << endl;
+    cin >> b;
+    cout << "Ingresa el valor de c" << endl;
+    cin >> c;
+    if  (a>=b and a>=c)
+    {
+        cout << "el numero mayor es " << a << endl;
+    }else{
+    if  (b>=a and b>=c )
+        {
+        cout << "El numero mayor es " << b << endl;
+    }else{
+        cout << "El numero mayor es " << c << endl;
+    }
+    }
     return 0;
 }
