e072191fa3f314fdbb28c92943d12cd2c355c47c
ALUMNO-E <ALUMNO-E@UCSP.AULAB>
2019-08-28 10:02:47 -0500

    project2

--- Mayordetresnumeros/main.cpp
+++ Mayordetresnumeros/main.cpp
@@ -25,4 +25,49 @@ int main()
     }
     }
     return 0;
+
+    /*
+int divisor=1;
+   int divisores=0;
+   int num;
+   cout <<"Ingrese un numero"<<endl;
+   cin>>num;
+
+   while(divisor<=num){
+        if(num%divisor==0){
+            divisores++;
+        }
+        divisor++;
+   }
+
+   if (divisores==2){
+    cout<<"Si es primo"<<endl;
+   }
+   else{
+    cout<<"No es primo"<<endl;
+   }
+
+   int n=1;
+   int divisor=1;
+   int divisores=0;
+
+   while(n<100){
+        while (divisor<=n){
+            if(n%divisor==0){
+                divisores++;
+            }
+            divisor++;
+        }
+
+        if(divisores==2){
+            cout<<n<<endl;
+        }
+        n++;
+        divisores=0;
+        divisor=1;
+
+
+
+
+   }
 }
