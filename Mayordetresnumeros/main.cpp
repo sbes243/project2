963861e7664ae250964de951453366af12d1b0ef
ALUMNO-E <ALUMNO-E@UCSP.AULAB>
2019-09-11 09:35:44 -0500

    completo

--- Mayordetresnumeros/main.cpp
+++ Mayordetresnumeros/main.cpp
@@ -25,6 +25,22 @@ int main()
    else{
     cout<<"No es primo"<<endl;
    }*/
+
+   /*int divisores=0;
+   int num;
+   cout <<"Ingrese un numero"<<endl;
+   cin>>num;
+   for(int divisor=1;divisor<=num;divisor++){
+    if(num%divisor==0){
+        divisores++;
+    }
+   }
+   if (divisores==2){
+    cout<<"Si es primo"<<endl;
+   }
+   else{
+    cout<<"No es primo"<<endl;}
+}*/
 /*
    int n=1;
    int divisor=1;
@@ -46,7 +62,27 @@ int main()
         divisor=1;
    }
    */
-   int n;
+
+  /* int divisores=0;
+   for(int n=0;n<100;n++){
+    for(int divisor=1;divisor<=n;divisor++){
+        if(n%divisor==0){
+                divisores++;
+            }
+            divisor++;
+        }
+
+        if(divisores==2){
+            cout<<n<<endl;
+        }
+
+        divisores=0;
+
+   }
+   }
+
+*/
+   /*int n;
     cout << "Ingrese un numero : " << endl;
     cin >>n;
     int divi=0;
@@ -64,5 +100,31 @@ int main()
         cout << "No es un numero perfecto" << endl;
     }
     return 0;
+}*/
+    int n;
+    cout << "Ingrese un numero : " << endl;
+    cin >>n;
+    int divi=0;
+    for(int sor=1;sor<n;sor++){
+        if(n%sor==0){
+            divi=divi+sor;
+        }
+    }
+    if(divi==n){
+        cout << "Es un numero perfecto" << endl;
+    }
+    else{
+        cout << "No es un numero perfecto" << endl;
+    }
+    return 0;
+    }
 
-}
+
+
+
+
+
+
+
+
+
